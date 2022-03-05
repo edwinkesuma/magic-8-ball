8a1e34ac5e3e759f2286e46b672771658b013640
origin <https://github.com/edwinkesuma/GetEat>
2022-03-05 23:06:36 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: BallPage(),
+      ),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
